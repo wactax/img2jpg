25f581018ee89a9b6d3dcc44e3eb9f9dda49ab61
Your Name <you@example.com>
2023-05-16 23:14:55 +0800

    ♨

--- dist.arm64.sh
+++ dist.arm64.sh
@@ -7,7 +7,7 @@ set -ex
 git pull
 yarn build --target aarch64-unknown-linux-gnu
 mv *.linux-arm64-gnu.node npm/linux-arm64-gnu
-jq ".version = $(cat package.json|jq .version)" -i npm/linux-arm64-gnu/package.json
+jq -i ".version = $(cat package.json|jq .version)" npm/linux-arm64-gnu/package.json
 cd npm/linux-arm64-gnu
 npm publish --access=public
 git checkout .
