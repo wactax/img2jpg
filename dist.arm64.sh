d77d3b4fee0d9cce179ef8aff1632d50e2231b18
Your Name <you@example.com>
2023-05-16 23:19:23 +0800

    ♨

--- dist.arm64.sh
+++ dist.arm64.sh
@@ -7,7 +7,12 @@ set -ex
 git pull
 yarn build --target aarch64-unknown-linux-gnu
 mv *.linux-arm64-gnu.node npm/linux-arm64-gnu
-jq -i ".version = $(cat package.json|jq .version)" npm/linux-arm64-gnu/package.json
+out=npm/linux-arm64-gnu/package.json
+if ! [ -x "$(command -v sponge)" ]; then
+  apt-get install -y moreutils
+fi
+jq -c ".version = $(cat package.json|jq .version)" $out | sponge $out
+mv .t npm/linux-arm64-gnu/package.json
 cd npm/linux-arm64-gnu
 npm publish --access=public
 git checkout .
